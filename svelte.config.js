a7af8d2e245df2e5287334909924289039b92c1e
krustle-03 <xeno112203@gmail.com>
2025-12-09 16:18:56 -0500

    sveltekit adapter

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see https://kit.svelte.dev/docs/adapter-static#options
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/CS5167_project-4' : '', // Adjust for your repository name
+		}
 	}
 };
 
