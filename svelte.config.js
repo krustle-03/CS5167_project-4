1e83b9d18ca9532eb28737089a6491c7e662df15
krustle-03 <xeno112203@gmail.com>
2025-12-09 16:31:09 -0500

    github pages workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see https://kit.svelte.dev/docs/adapter-static#options
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/CS5167_project-4' : '', // Adjust for your repository name
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
